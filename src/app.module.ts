7bd5269a63f8cd9bb63c99570d56711eb8a7d40a
M Fazli Anan <fazlianan321@gmail.com>
2026-04-09 09:26:25 +0700

    fix(dto): resolve parsing error and add proper initializers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KategoriModule } from './kategori/kategori.module';
+import { WisataModule } from './wisata/wisata.module';
 
 @Module({
-  imports: [KategoriModule],
+  imports: [KategoriModule, WisataModule],
   controllers: [AppController],
   providers: [AppService],
 })
